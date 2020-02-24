5df508caefc9b785a3d9e205c8a159ec5bfd1cb5
Spitfire-Ice <37056025+Spitfire-Ice@users.noreply.github.com>
2020-02-24 22:58:17 +0200

    add menu button

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,14 @@ import './menu';
 import '../../node_modules/smooth-scroll/dist/smooth-scroll.polyfills'
 import './plugins';
 
+
+const menuToggleAction = ()=> {
+  if (window.innerWidth <= 850) {
+    const addButtonChange = document.getElementById('mobileButton');
+    addButtonChange.classList.toggle('buttonChange');
+    const menuActionSlide = document.getElementById("navigation");
+    menuActionSlide.classList.toggle("menuAction");
+  }
+};
+window.menuToggleAction=menuToggleAction;
+
